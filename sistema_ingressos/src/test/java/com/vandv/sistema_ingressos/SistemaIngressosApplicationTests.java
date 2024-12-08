ef0266917d21ab98a41dd67e41363ad0322f3438
lucas-s-p <lucas.pereira@ccc.ufcg.edu.br>
2024-12-08 19:47:51 -0300

    chore: configure packages and initial structure

---
+++ sistema_ingressos/src/test/java/com/vandv/sistema_ingressos/SistemaIngressosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vandv.sistema_ingressos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaIngressosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
