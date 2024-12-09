de0626a5dba9e488b65dd81d84394ea03cceab17
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2024-12-09 19:03:19 -0300

    chore: configure packages and initial structure.

---
+++ processador_contas/src/main/java/vev/processador_contas/ProcessadorContasApplication.java
@@ -0,0 +1,13 @@
+package vev.processador_contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessadorContasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessadorContasApplication.class, args);
+    }
+
+}
